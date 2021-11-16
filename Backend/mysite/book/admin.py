0791b61b9891d1f31000d8d77379dd2d46c4bdea
Alireza Maleki <Alirezamalekiiiii78@gmail.com>
2021-11-16 23:45:16 +0330

    add templates, forms

--- Backend/mysite/book/admin.py
+++ Backend/mysite/book/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product
+
+from .models import Item
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Item)
--- Backend/mysite/book/models.py
+++ Backend/mysite/book/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
-class Product(models.Model):
+class Item(models.Model):
+    
     
     def __str__(self):
-        return self.product_name
+        return self.item_name
     
-    product_name = models.CharField(max_length=200)
-    product_desc = models.CharField(max_length=200)
-    product_price = models.IntegerField()
+    
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://www.kindpng.com/picc/m/725-7251301_book-cover-placeholder-hd-png-download.png")
+    
+
